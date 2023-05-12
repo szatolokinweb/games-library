68a94c3f4ae11ef9aefa4ba315db5fffb73fafb6
szatolokinweb <szatolokin.web@gmail.com>
2023-05-13 01:47:54 +0300

    add search model

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,4 +1,9 @@
+import { useMemo } from "react";
+import { Search as SearchModel } from "../../models/search";
+
 const Search: React.FC = () => {
+    const searchModel = useMemo(() => new SearchModel(), []);
+
     return <h1>search</h1>;
 };
 
---
+++ src/models/search.ts
@@ -0,0 +1,9 @@
+import { makeObservable } from "mobx";
+
+class Search {
+    constructor() {
+        makeObservable(this);
+    }
+}
+
+export { Search };
