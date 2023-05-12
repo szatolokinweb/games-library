1b5b7b284fae78a09420e257e286d8cceb11f53c
szatolokinweb <szatolokin.web@gmail.com>
2023-05-13 02:24:44 +0300

    add antd, setFilter, mobx-react

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,19 +1,20 @@
 import { useMemo } from "react";
 import { useSearchParams } from "react-router-dom";
+import { Input } from "antd";
+import { observer } from "mobx-react";
 import { Search as SearchModel } from "../../models/search/search";
 
-const Search: React.FC = () => {
+const Search: React.FC = observer(() => {
     const [searchParams, setSearchParams] = useSearchParams();
 
     const searchModel = useMemo(() => new SearchModel(searchParams), []);
 
     return (
-        <>
-            {searchModel.filter.genres.map((genre) => (
-                <div key={genre}>{genre}</div>
-            ))}
-        </>
+        <Input
+            value={searchModel.filter.title}
+            onChange={(e) => searchModel.setFilter({ title: e.target.value })}
+        />
     );
-};
+});
 
 export { Search };
--- src/models/search/search.ts
+++ src/models/search/search.ts
@@ -1,4 +1,4 @@
-import { makeObservable, observable, toJS } from "mobx";
+import { makeObservable, observable, action } from "mobx";
 import { Filter, getFilterFromSearchParams } from "./filter";
 
 class Search {
@@ -7,9 +7,14 @@ class Search {
     constructor(searchParams: URLSearchParams) {
         makeObservable(this, {
             filter: observable,
+            setFilter: action,
         });
 
         this.filter = getFilterFromSearchParams(searchParams);
+    }
+
+    setFilter(filter: Partial<Filter>) {
+        this.filter = Object.assign(this.filter, filter);
     }
 }
 
